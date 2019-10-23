34185e7c9bf19591b5594661fc7322208b61b2a5
alycia hall <amhall25@gmail.com>
2019-10-23 23:04:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :employeediscount
   
-  def initialize(discount=0.0)
+  def initialize(discount=0)
     @total=0.0
     @employeediscount=discount
   end
